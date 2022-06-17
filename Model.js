b642df82736728bf1d78fce11fd1ef44767c2410
tipsun91 <tipsun911@gmail.com>
2022-06-17 12:13:25 +0300

    readdir done

--- Model.js
+++ Model.js
@@ -1,6 +1,21 @@
 class Model {
-  constructor() {
+  #topicPath = './topics/';
+  topics;
 
+  constructor(fsp) {
+    this.fsp = fsp;
+  }
+
+  async getTopics() {
+    this.topics = await this.fsp.readdir(this.#topicPath);
+    return this.topics;
+  }
+
+  async getQuestions(topicNumber) {
+    const filepath = this.#topicPath + this.topics[topicNumber - 1];
+    let tmp = await this.fsp.readFile(filepath, 'utf-8');
+    this.questions = tmp.trim().split('\n\n').map(c => c.split('\n'));
+    return this.questions;
   }
 }
 
